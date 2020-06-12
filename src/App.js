7abe8004af67f50e986d9acfe42a950231e7ac3b
an4morph <avgustin456@gmail.com>
2020-06-12 17:35:53 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Route, BrowserRouter, Switch } from 'react-router-dom'
+import Todolist from './pages/Todolist'
+import NotFoundPage from './pages/NotFound'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" component={Todolist} exact />
+        <Route component={NotFoundPage} />
+      </Switch>
+    </BrowserRouter>
+  )
+}
+
+export default App
